82567091e007b5dc09550e73f44fe814d0c1a8e1
Akshay Kumar <155633263+akshay7549@users.noreply.github.com>
2024-11-20 14:22:48 +0530

    Libraries files

---
+++ models.py
@@ -0,0 +1,23 @@
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+
+db = SQLAlchemy()
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
+class Video(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    category = db.Column(db.String(100), nullable=False)
+    filename = db.Column(db.String(200), nullable=False)
+    uploader_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    uploader = db.relationship('User', backref='videos')
